113ffee4eff3d7285424b6dac967653f83b59737
Twana Jirjees <twanalearner@gmail.com>
2023-09-09 03:01:36 +0300

    first commit

---
+++ lib/pages/choose_location.dart
@@ -0,0 +1,34 @@
+import 'package:flutter/material.dart';
+
+class ChooseLocation extends StatefulWidget {
+  const ChooseLocation({super.key});
+
+  @override
+  State<ChooseLocation> createState() => _ChooseLocationState();
+}
+
+class _ChooseLocationState extends State<ChooseLocation> {
+  int counter = 0;
+
+  @override
+  Widget build(BuildContext context) {
+    print('build function run');
+    return Scaffold(
+      appBar: AppBar(
+        backgroundColor: Colors.blue[900],
+        title: const Text('Choose a Location'),
+        centerTitle: true,
+        elevation: 0,
+      ),
+      backgroundColor: Colors.grey[200],
+      body: ElevatedButton(
+        onPressed: () {
+          setState(() {
+            counter++;
+          });
+        },
+        child: Text('counter $counter'),
+      ),
+    );
+  }
+}
---
+++ lib/pages/loading.dart
@@ -0,0 +1,38 @@
+import 'package:flutter/material.dart';
+import 'package:http/http.dart';
+import 'dart:convert';
+
+class Loading extends StatefulWidget {
+  const Loading({super.key});
+
+  @override
+  State<Loading> createState() => _LoadingState();
+}
+
+class _LoadingState extends State<Loading> {
+  void getTime() async {
+    Response response = await get(
+        Uri.parse('https://worldtimeapi.org/api/timezone/Europe/London'));
+    Map data = jsonDecode(response.body);
+
+    String offset = data['utc_offset'].substring(1, 3);
+    String datetime = data['datetime'];
+    DateTime now = DateTime.parse(datetime);
+    now = now.add(Duration(hours: int.parse(offset)));
+    print(now);
+
+  }
+
+  @override
+  void initState() {
+    super.initState();
+    getTime();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return const Scaffold(
+      body: Text('loading screen'),
+    );
+  }
+}
