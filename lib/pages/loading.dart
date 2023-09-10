c0170e9eb9ded810206230eb3e30631d6491f876
Twana Jirjees <twanalearner@gmail.com>
2023-09-11 02:09:19 +0300

    home screen time flag location

--- lib/pages/loading.dart
+++ lib/pages/loading.dart
@@ -9,17 +9,21 @@ class Loading extends StatefulWidget {
 }
 
 class _LoadingState extends State<Loading> {
-
-  String time = 'loading';
+  // String time = 'loading';
 
   void setupWorldTime() async {
     WorldTime instance = WorldTime(
         location: 'Berlin', flag: 'germany.png', url: 'Europe/Berlin');
     await instance.getTime();
-    print(instance.time);
-   setState(() {
-     time = instance.time;
-   });
+    Navigator.pushReplacementNamed(context, '/home', arguments: {
+      'location': instance.location,
+      'flag': instance.flag,
+      'time' : instance.time
+    });
+    //  print(instance.time);
+    // setState(() {
+    //   time = instance.time;
+    // });
   }
 
   @override
@@ -30,10 +34,10 @@ class _LoadingState extends State<Loading> {
 
   @override
   Widget build(BuildContext context) {
-    return  Scaffold(
+    return Scaffold(
       body: Padding(
         padding: EdgeInsets.all(50.0),
-        child: Text(time),
+        child: Text('Loading'),
       ),
     );
   }
