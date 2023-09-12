a015af0d8f7d46ffc28154716966bac92f824d67
Twana Jirjees <twanalearner@gmail.com>
2023-09-13 01:02:07 +0300

    packages and new update

--- lib/pages/loading.dart
+++ lib/pages/loading.dart
@@ -12,18 +12,13 @@ class _LoadingState extends State<Loading> {
   // String time = 'loading';
 
   void setupWorldTime() async {
-    WorldTime instance = WorldTime(
-        location: 'Berlin', flag: 'germany.png', url: 'Europe/Berlin');
+    WorldTime instance = WorldTime(location: 'Berlin', flag: 'germany.png', url: 'Europe/Berlin');
     await instance.getTime();
     Navigator.pushReplacementNamed(context, '/home', arguments: {
       'location': instance.location,
       'flag': instance.flag,
       'time' : instance.time
     });
-    //  print(instance.time);
-    // setState(() {
-    //   time = instance.time;
-    // });
   }
 
   @override
--- lib/services/world_time.dart
+++ lib/services/world_time.dart
@@ -1,5 +1,6 @@
 import 'package:http/http.dart';
 import 'dart:convert';
+import 'package:intl/intl.dart';
 
 class WorldTime {
   late String time;
@@ -10,15 +11,22 @@ class WorldTime {
   WorldTime({ required this.url, required this.flag, required this.location});
 
   Future<void> getTime() async {
-    Response response = await get(
-        Uri.parse('https://worldtimeapi.org/api/timezone/$url'));
-    Map data = jsonDecode(response.body);
+    try{
+      Response response = await get(
+          Uri.parse('https://worldtimeapi.org/api/timezone/$url'));
+      Map data = jsonDecode(response.body);
+      String offset = data['utc_offset'].substring(1, 3);
+      String datetime = data['datetime'];
+      DateTime now = DateTime.parse(datetime);
+      now = now.add(Duration(hours: int.parse(offset)));
+      // time = now.toString();
+      time = DateFormat().add_jm().format(now);
+      print(time);
+    }catch (e){
+      print('caught error: $e');
+      time = 'could not get time data';
+    }
 
-    String offset = data['utc_offset'].substring(1, 3);
-    String datetime = data['datetime'];
-    DateTime now = DateTime.parse(datetime);
-    now = now.add(Duration(hours: int.parse(offset)));
-    time = now.toString();
   }
 }
 
